dbb52873736d4b6fc6f6c46676cce35016f21efd
ural <sevomax@gmail.com>
2018-02-10 20:31:21 -0500

    playing with throw error

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,19 +4,22 @@ import './person.css';
 
 const Person = (props) => {
 
-/*
-    const pStyle = {
-      '@media (min-width: 768px)': {
-          background: '#af580b'
-      }
-    };
-*/
+
+    const randomN = Math.round(Math.random() * 10);
+    if (randomN > 7 ) {
+        console.log(randomN);
+        throw new Error("What's going ON?????");
+    }
 
     return (
         <div className="person">
             <p onClick={props.clickity} >I&#39;m  {props.name} and I am {props.age} years old.</p>
             <p> {props.children} </p>
             <input type="text" onChange={props.changed} placeholder="Type New Name" />
+            <hr/>
+            <span>
+                random number: {randomN}
+            </span>
         </div>
         );
 };
