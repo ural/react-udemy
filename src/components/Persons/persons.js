da5932d2207f8fa2df4010508dcb44c464e1cd61
ural <sevomax@gmail.com>
2018-02-11 22:16:41 -0500

    added Cockpit.js component this type for real

---
+++ src/components/Persons/persons.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Person from './Person/Person';
+
+const persons = (props) => props.persons.map((person, index) => {
+    return <Person
+      clickity={() => props.clicked(index)}
+      key={person.id}
+      name={person.name}
+      age={person.age}
+      changedName={(event) => props.changed(event, person.id)}
+    />
+  });
+
+export default persons;
